2f0b66c559855a426904c16084f925e0d8d53f93
Gabriel Borges <gabriel.borges@gwcloud.company>
2024-05-16 00:59:54 +0000

    create repository

---
+++ solidarize/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/index.module.scss";
+
+export default function index() {
+    return (
+        <>
+        <Head>
+            <div className={styles.menu}>
+            </div>
+        </Head> 
+
+        </>
+    );
+}
