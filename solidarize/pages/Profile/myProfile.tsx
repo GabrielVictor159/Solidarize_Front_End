3f685104348d70573ea1cc40b2496dd3227325a0
GabrielVictor159 <gabrielvictor159487@gmail.com>
2024-05-28 21:02:39 +0000

    developing myProfile page

--- solidarize/pages/Profile/myProfile.tsx
+++ solidarize/pages/Profile/myProfile.tsx
@@ -2,42 +2,108 @@
 import LoginDefault from "@/layouts/Login/loginDefault";
 import { useEffect, useRef, useState } from "react";
 import styles from "@/styles/pages/Profile/myProfile.module.scss";
-import { Button, Image, Input } from "@nextui-org/react";
+import { Button, image, Image, Input } from "@nextui-org/react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/Provider/Store";
 import { useRouter } from "next/router";
+import clsx from 'clsx';
 export default function MyProfile() {
-  const [image, setImage] = useState<string | URL>();
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [icon, setIcon] = useState<string | URL>();
+  const iconInputRef = useRef<HTMLInputElement>(null);
   const loginResponse = useSelector((state: RootState) => state.loginResponse);
   const router = useRouter();
+  const [images, setImages] = useState<string[]>([]);
+  const imagesInputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(()=>{
-    if(loginResponse.UserInformation===undefined){
-        router.push("/Login");
+  useEffect(() => {
+    console.log(loginResponse.UserInformation);
+    if (loginResponse.UserInformation === undefined) {
+      router.push("/Login");
     }
-  },[]);
-  
+    if (
+      loginResponse.UserInformation?.Images !== null &&
+      loginResponse.UserInformation?.Images !== undefined
+    ) {
+      let imagesNewArray:string[] = [];
+      loginResponse.UserInformation?.Images?.forEach((item) => {
+        imagesNewArray.push(
+          `${process.env.NEXT_PUBLIC_CONTAINER_IMAGE}${item.split(".")[0]}`
+        );
+      });
+      setImages(imagesNewArray);
+    }
+  }, []);
+
   return (
     <>
       <LoginDefault imageActive={false} center={true}>
         <>
           <div
-            onClick={() => fileInputRef.current?.click()}
-            className={styles.Image_View_Container}
+            onClick={() => iconInputRef.current?.click()}
+            className={styles.Icon_View_Container}
           >
-            {image === undefined ? (
-              <div className={styles.Image_View_Container_background}/>
+            {icon === undefined &&
+            (loginResponse.UserInformation?.Icon === null ||
+              loginResponse.UserInformation?.Icon === "") ? (
+              <div className={styles.Icon_View_Container_background} />
             ) : (
-              <Image alt="Imagem do Usuario" src={image.toString()} />
+              <Image
+                alt="Imagem do Usuario"
+                className={styles.Icon_View_Container_Image}
+                src={
+                  icon === undefined
+                    ? `${process.env.NEXT_PUBLIC_CONTAINER_IMAGE}${
+                        loginResponse.UserInformation?.Icon!.split(".")[0]
+                      }`
+                    : icon.toString()
+                }
+              />
             )}
+          </div>
+          <br />
+          <div className={styles.Images_View}>
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className={clsx(styles.Images_View_ImagesContainer_Itens,styles.Images_View_ImagesContainer)}
+                onClick={() => {
+                  setImages(
+                    images.filter((item, indexItem) => indexItem !== index)
+                  );
+                }}
+              >
+                <Image
+                  alt="Imagem do Usuario"
+                  className={styles.Images_View_ImagesContainer_Images}
+                  src={image}
+                />
+              </div>
+            ))}
+            <div className={clsx(styles.Images_View_ImagesContainer, styles.Images_View_ImagesContainer_AddItens)}>
+            <Image
+                  alt="Imagem do Usuario"
+                  className={styles.Images_View_ImagesContainer_ImagePushItems}
+                  src={"/2803242.jpg"}
+                />
+            </div>
           </div>
           <input
             type="file"
-            ref={fileInputRef}
+            ref={iconInputRef}
             onChange={(e) => {
               if (e.target.files && e.target.files[0]) {
-                setImage(URL.createObjectURL(e.target.files[0]));
+                setIcon(URL.createObjectURL(e.target.files[0]));
+              }
+            }}
+            accept="image/png, image/jpeg"
+            style={{ display: "none" }}
+          />
+          <input
+            type="file"
+            ref={imagesInputRef}
+            onChange={(e) => {
+              if (e.target.files && e.target.files[0]) {
+                
               }
             }}
             accept="image/png, image/jpeg"
